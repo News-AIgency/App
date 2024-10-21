fc6c403341d95b1af849ce0313646e2bfb6e931d
Tomas Tomcany <tomcany.tomas@gmail.com>
2024-10-21 19:37:48 +0200

    Fixed jina_scraper according to Ruff + Black

--- backend/app/services/scraping_service/jina_scraper.py
+++ backend/app/services/scraping_service/jina_scraper.py
@@ -1,6 +1,7 @@
-import requests
 import aiohttp
-from validators import url as validate_url, ValidationError
+import requests
+from validators import ValidationError
+from validators import url as validate_url
 
 
 def check_url(url: str) -> bool:
@@ -11,7 +12,8 @@ def check_url(url: str) -> bool:
 
     return result
 
-async def jina_scrape(scrape_url: str) -> (str | None):
+
+async def jina_scrape(scrape_url: str) -> str | None:
 
     if not check_url(scrape_url):
         raise ValueError("The argument is not a valid URL")
@@ -23,9 +25,11 @@ async def jina_scrape(scrape_url: str) -> (str | None):
     }
 
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as response:
-                response.raise_for_status()
-                return await response.text()
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(url, headers=headers) as response,
+        ):
+            response.raise_for_status()
+            return await response.text()
     except requests.RequestException as e:
         raise Exception(status_code=500, detail=str(e))

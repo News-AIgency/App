da9385fb1ff78675a9f6a742f1d963b71e181543
totes8 <86833007+totes8@users.noreply.github.com>
2024-10-23 18:53:41 +0200

    Router + Views setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
 const app = createApp(App)
 
 app.use(createPinia())

c8f2edf54b0a8c27aa9a4a6d18d8f7e697eeef2f
TomasTomcany <61255352+Smitomic@users.noreply.github.com>
2024-11-18 17:38:07 +0100

    Convert url to topic endpoint to dev/production environment

--- backend/app/api/routes/url_to_topic.py
+++ backend/app/api/routes/url_to_topic.py
@@ -1,9 +1,11 @@
+from typing import Any, Coroutine
 from urllib.parse import urlparse
 
 from fastapi import APIRouter, HTTPException, Request
 from fastapi_cache import FastAPICache
 from starlette.responses import JSONResponse
 
+from backend.app.core.config import settings
 from backend.app.services.ai_service.litellm_service import LiteLLMService
 from backend.app.services.ai_service.response_models import TopicsResponse
 from backend.app.services.scraping_service.jina_scraper import jina_scrape
@@ -11,9 +13,8 @@ from backend.app.utils.default_article import default_article, default_article_u
 
 router = APIRouter()
 
-
-@router.get("/url-validate")
-async def url_validate_get(
+# region Url domain validation endpoint
+async def url_validate(
     url: str = default_article_url,
     hostnames: list[str] = None,
 ) -> JSONResponse:
@@ -32,6 +33,13 @@ async def url_validate_get(
         raise HTTPException(status_code=400, detail="Invalid URL")
     return JSONResponse(content={"detail": url})
 
+if settings.ENVIRONMENT == "development":
+    @router.get("/url-validate")
+    async def url_validate_get(
+        url: str = default_article_url,
+        hostnames: list[str] = None,
+    ) -> JSONResponse:
+        return await url_validate(url)
 
 @router.post("/url-validate")
 async def url_validate_post(
@@ -39,24 +47,12 @@ async def url_validate_post(
 ) -> JSONResponse:
     request_body = await request.json()
     url = request_body.get("url", default_article_url)
-    hostnames = request_body.get(
-        "hostnames", ["slovak.statistics.sk"]
-    )  # List of trustworthy sources
-
-    try:
-        result = urlparse(url)
-        if result.hostname not in hostnames:
-            raise HTTPException(
-                status_code=400,
-                detail=f"The hostname {result.hostname} is not allowed.",
-            )
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid URL")
-    return JSONResponse(content={"detail": url})
+    return await url_validate(url)
+# endregion
 
 
-@router.get("/article/topics", response_model=TopicsResponse)
-async def extract_topics_get(
+# region Extract topics endpoint
+async def extract_topics(
     url: str = default_article,
 ) -> TopicsResponse:
     try:
@@ -86,7 +82,6 @@ async def extract_topics_get(
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
 @router.post("/article/topics", response_model=TopicsResponse)
 async def extract_topics_post(
     request: Request,
@@ -94,28 +89,15 @@ async def extract_topics_post(
     try:
         request_body = await request.json()
         url = request_body.get("url", default_article)
-
-        cache_key = f"article:{url}"
-        default_cache_key = f"article:{default_article_url}"
-        cached_content = await FastAPICache.get_backend().get(cache_key)
-
-        if not cached_content:
-            if url == default_article:
-                scraped_content = url
-                await FastAPICache.get_backend().set(
-                    default_cache_key, scraped_content, expire=3600
-                )
-            else:
-                scraped_content = await jina_scrape(url)
-                await FastAPICache.get_backend().set(
-                    cache_key, scraped_content, expire=3600
-                )
-        else:
-            scraped_content = cached_content
-
-        ai_service = LiteLLMService()
-        generated_topics = await ai_service.generate_topics(scraped_content)
-
-        return generated_topics
+        return await extract_topics(url)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+if settings.ENVIRONMENT == "development":
+    @router.get("/article/topics", response_model=TopicsResponse)
+    async def extract_topics_get(
+    url: str = default_article,
+    ) -> TopicsResponse:
+        return await extract_topics(url)
+# endregion

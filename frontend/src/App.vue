64eaee064e03c3cda7b9eae796ed116e2ba749cf
totes8 <86833007+totes8@users.noreply.github.com>
2024-12-21 11:37:55 +0100

    updated sidebar + logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,36 @@
-<script setup lang="ts">
-import { RouterView, useRouter } from 'vue-router'
-
-const router = useRouter();
-
-function goToStart() {
-  router.push('/');
-}
-
-function mailTo() {
-  window.location.href = 'mailto:tptim042024@gmail.com';
-}
-</script>
 
 <template>
   <div class="page-wrapper">
     <nav class="navbar">
-      <a class="navbar-logo" @click="goToStart">WR<span class="text-accent">AI</span>TE</a>
+      <!-- <a class="navbar-logo" @click="goToStart"
+        >WR<span class="text-accent">AI</span>TE</a
+      > -->
+      <img src="../public/logo.svg" alt="WrAIte XMAS logo <3">
+      <div class="navbar-item"><a href="">Home</a></div>
+      <div class="navbar-item"><a href="">Organization</a></div>
+      <div class="navbar-item"><a href="">History</a></div>
       <a class="contact-us" @click="mailTo">Contact Us</a>
     </nav>
-    <RouterView />
-    <footer class="footer-container">
-      <p>News AIgency</p>
-      <p>2024</p>
-    </footer>
+    <div class="content-wrapper"><RouterView /></div>
   </div>
 </template>
 
+<script setup lang="ts">
+import { RouterView, useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToStart() {
+  router.push('/')
+}
+
+function mailTo() {
+  window.location.href = 'mailto:tptim042024@gmail.com'
+}
+</script>
+
 <style scoped>
-@media screen and (max-device-width:480px) {
+@media screen and (max-device-width: 480px) {
   .navbar {
     gap: 10%;
   }
@@ -38,19 +41,41 @@ function mailTo() {
 }
 
 .navbar {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  gap: 8px;
   color: var(--color-text);
+  width: 25%;
+  height: 100vh;
 }
 
 .navbar-logo {
   cursor: pointer;
 }
+.navbar-item {
+  background-color: var(--color-block);
+  border-radius: 10px;
+  padding: 8px 4px;
+  width: 170px;
+}
+
+.navbar-item a {
+  color: var(--color-text-haze);
+  font-weight: 600;
+  text-decoration: none;
+}
 
 .page-wrapper {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   min-height: 100vh;
   position: relative;
   padding-bottom: 2.5rem;
+}
+
+.content-wrapper {
+  width: 100%;
 }
 
 .contact-us {

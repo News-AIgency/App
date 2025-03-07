7379748ab567039f2cb5f53e28c9499979ae9505
TomasTomcany <61255352+Smitomic@users.noreply.github.com>
2025-03-07 20:18:04 +0100

    Added llm_as_judge scores & evaluation aggregated by news sites

--- backend/app/iitsrc/csv_generator.py
+++ backend/app/iitsrc/csv_generator.py
@@ -5,8 +5,10 @@ import random
 import pandas as pd
 
 from backend.app.iitsrc.article_response_converter import check_origin_url
+from backend.app.iitsrc.dspy_llm_judge import llm_compare_strings
 from backend.app.services.ai_service.litellm_service import LiteLLMService
-from backend.app.services.ai_service.storm_agent.STORM_service import run_storm
+
+# from backend.app.services.ai_service.storm_agent.STORM_service import run_storm
 from backend.app.services.scraping_service.jina_scraper import jina_scrape
 
 
@@ -43,7 +45,7 @@ def generate_original_article_csv() -> None:
                 writer.writerow([sheet_name, headline, engaging_text, article, tags])
 
 
-async def generate_generated_article_csv(storm: bool=False) -> None:
+async def generate_generated_article_csv(storm: bool = False) -> None:
     file_path = "articles.xlsx"
     output_csv = "generated_articles_storm.csv" if storm else "generated_articles.csv"
 
@@ -52,7 +54,7 @@ async def generate_generated_article_csv(storm: bool=False) -> None:
 
     with open(output_csv, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file, delimiter=";")
-        writer.writerow(["Sheet Name", "Headline", "Engaging Text", "Article", "Tags"])
+        writer.writerow(["News Site", "Headline", "Engaging Text", "Article", "Tags"])
 
         for sheet_name in xls.sheet_names:
             df = pd.read_excel(
@@ -74,7 +76,8 @@ async def generate_generated_article_csv(storm: bool=False) -> None:
                 )
                 print("headline complete")
 
-                if storm:
+                """
+                                if storm:
                     storm_article = run_storm(selected_topic, url)
                     print("storm article complete")
 
@@ -96,18 +99,20 @@ async def generate_generated_article_csv(storm: bool=False) -> None:
                     )
                     print("article complete")
                 else:
-                    engaging_text_response = await llm_service.generate_engaging_text(
-                        scraped_content=scraped_content,
-                        selected_topic=selected_topic,
-                        current_headline=headline_response.headlines[0],
-                    )
-                    print("engaging text complete")
-                    article_response = await llm_service.generate_article_body(
-                        scraped_content=scraped_content,
-                        selected_topic=selected_topic,
-                        current_headline=headline_response.headlines[0],
-                    )
-                    print("article complete")
+                """
+
+                engaging_text_response = await llm_service.generate_engaging_text(
+                    scraped_content=scraped_content,
+                    selected_topic=selected_topic,
+                    current_headline=headline_response.headlines[0],
+                )
+                print("engaging text complete")
+                article_response = await llm_service.generate_article_body(
+                    scraped_content=scraped_content,
+                    selected_topic=selected_topic,
+                    current_headline=headline_response.headlines[0],
+                )
+                print("article complete")
 
                 tags_response = await llm_service.generate_tags(
                     scraped_content=scraped_content,
@@ -129,7 +134,27 @@ async def generate_generated_article_csv(storm: bool=False) -> None:
                 print("Done")
 
 
+async def generate_llm_as_judge_csv() -> None:
+    original_articles = pd.read_csv("original_articles.csv", delimiter=";")
+    generated_articles = pd.read_csv("generated_articles.csv", delimiter=";")
+
+    original_article_section = original_articles["Article"]
+    generated_article_section = generated_articles["Article"]
+
+    scores = []
+    print(original_article_section)
+    for original_article, generated_article in zip(
+        original_article_section, generated_article_section
+    ):
+        score = await llm_compare_strings(original_article, generated_article)
+        scores.append(score)
+
+    scores_df = pd.DataFrame(scores, columns=["LLM as judge score"])
+    scores_df.to_csv("llm_as_judge_scores.csv", index=False)
+
+
 if __name__ == "__main__":
     # generate_original_article_csv()
     # asyncio.run(generate_generated_article_csv())
-    asyncio.run(generate_generated_article_csv(True))
+    # asyncio.run(generate_generated_article_csv(True))
+    asyncio.run(generate_llm_as_judge_csv())

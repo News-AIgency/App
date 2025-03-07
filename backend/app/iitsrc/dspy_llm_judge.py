d67221a6a94ecf719d43207ba20f14d999b8dc3f
Jakub Grúber <jakubgruber90@gmail.com>
2025-03-07 17:20:10 +0100

    Created LLM judge for string comparison

---
+++ backend/app/iitsrc/dspy_llm_judge.py
@@ -0,0 +1,38 @@
+import dspy
+from backend.app.core.config import settings
+
+lm = dspy.LM(
+            "openai/gpt-4o-mini",
+            api_key=settings.LITE_LLM_KEY,
+            base_url="http://147.175.151.44/",
+        )
+dspy.settings.configure(lm=lm, async_max_workers=8)
+
+class LlmJudgeSignature(dspy.Signature):
+    """Compare the two strings str_1 and str_2. Output a float, similarity, that represents how close or similar the two strings are. The similarity output must be a float between 0.0 and 1.0. The higher the number the more similar the two strings are. If the are nothing alike, it should be 0.0, if the are totally the same, it should be 1.0."""
+    
+    str_1 = dspy.InputField(type=str, desc="The first string to compare")
+    str_2 = dspy.InputField(type=str, desc="The second string to compare")
+    similarity = dspy.OutputField(type=float, desc="The similarity of the two input strings from 0.0 to 1.0")
+
+class LlmJudge(dspy.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.compare_strings = dspy.Predict(LlmJudgeSignature)
+
+    def forward(
+        self,
+        str_1: str,
+        str_2: str
+    ) -> LlmJudgeSignature:
+        response = self.compare_strings(
+            str_1=str_1,
+            str_2=str_2
+        )
+        return response.similarity
+    
+async def llm_compare_strings(string_1: str, string_2: str) -> float:
+    compare_string_program = LlmJudge()
+    compare_string_program = dspy.asyncify(compare_string_program)
+    similarity = await compare_string_program(string_1, string_2)
+    return similarity

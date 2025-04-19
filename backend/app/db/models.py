da226905ee9e834fc7fff374a5931cda79785efa
JakubGasparin <55839847+JakubGasparin@users.noreply.github.com>
2025-04-19 19:16:31 +0200

    Update models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey, Float, JSON
 from sqlalchemy.orm import relationship
 
 from database import Base
@@ -13,27 +13,29 @@ article_tags = Table(
     Column("tag_id", Integer, ForeignKey("tags.id"), primary_key=True),
 )
 
-
 class GeneratedArticles(Base):
     __tablename__ = "generated_articles"
     id = Column(Integer, primary_key=True, index=True)
 
-    heading_id = Column(Integer, ForeignKey("headings.id"), nullable=True)
+    headings_id = Column(Integer, ForeignKey("headings.id"), nullable=True)
     topic_id = Column(Integer, ForeignKey("topics.id"), nullable=True)
-    text_id = Column(Integer, ForeignKey("texts.id"), nullable=True)
+   # text_id = Column(Integer, ForeignKey("texts.id"), nullable=True)
     body_id = Column(Integer, ForeignKey("body.id"), nullable=True)
     perex_id = Column(Integer, ForeignKey("perex.id"), nullable=True)
+    engaging_text_id = Column(Integer, ForeignKey("engaging_texts.id"), nullable=True)
+    graph_data_id = Column(Integer, ForeignKey("graph_data.id"), nullable=True)
+    tags_id = Column(Integer, ForeignKey("tags.id"), nullable=True)
     # images_id = Column(Integer, ForeignKey("images.id"), nullable=True)
     sources_id = Column(Integer, ForeignKey("sources.id"), nullable=True)
 
-    heading = relationship(
-        "Heading", back_populates="article", foreign_keys=[heading_id]
-    )
+    heading = relationship("Heading",back_populates="article", foreign_keys=[headings_id])
     topic = relationship("Topic", back_populates="article", foreign_keys=[topic_id])
-    text = relationship("Text", back_populates="article", foreign_keys=[text_id])
+   # text = relationship("Text", back_populates="article", foreign_keys=[text_id])
     body = relationship("Body", back_populates="article", foreign_keys=[body_id])
     perex = relationship("Perex", back_populates="article", foreign_keys=[perex_id])
-    tags = relationship("Tags", secondary=article_tags, back_populates="articles")
+    tags = relationship("Tags", secondary=article_tags, back_populates="article")
+    engaging_text = relationship("EngagingText", back_populates="article", foreign_keys=[engaging_text_id])
+    graph_data = relationship("GraphData", back_populates="article", foreign_keys=[graph_data_id])
     # images = relationship("Images", back_populates="article", foreign_keys=[images_id])
     sources = relationship(
         "Sources", back_populates="article", foreign_keys=[sources_id]
@@ -58,11 +60,33 @@ class Heading(Base):
     article = relationship(
         "GeneratedArticles",
         back_populates="heading",
-        foreign_keys=[GeneratedArticles.heading_id],
+        foreign_keys=[GeneratedArticles.headings_id],
+    )
+
+class Tags(Base):
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tags_content = Column(String, index=True, nullable=False)
+    article = relationship(
+        "GeneratedArticles",
+        back_populates="tags",
+        foreign_keys=[GeneratedArticles.tags_id],
+    )
+
+
+class EngagingText(Base):
+    __tablename__ = "engaging_texts"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    engaging_text_content = Column(String, index=True, nullable=False)
+    article = relationship(
+        "GeneratedArticles",
+        back_populates="engaging_text",
+        foreign_keys=[GeneratedArticles.engaging_text_id],
     )
 
 
 class Topic(Base):
+
     __tablename__ = "topics"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     topic_content = Column(String, index=True, nullable=False)
@@ -73,15 +97,15 @@ class Topic(Base):
     )
 
 
-class Text(Base):
-    __tablename__ = "texts"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    text_content = Column(String, index=True, nullable=False)
-    article = relationship(
-        "GeneratedArticles",
-        back_populates="text",
-        foreign_keys=[GeneratedArticles.text_id],
-    )
+# class Text(Base):
+#     __tablename__ = "texts"
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     text_content = Column(String, index=True, nullable=False)
+#     article = relationship(
+#         "GeneratedArticles",
+#         back_populates="text",
+#         foreign_keys=[GeneratedArticles.text_id],
+#     )
 
 
 class Body(Base):
@@ -105,14 +129,20 @@ class Perex(Base):
         foreign_keys=[GeneratedArticles.perex_id],
     )
 
-
-class Tags(Base):
-    __tablename__ = "tags"
+class GraphData(Base):
+    __tablename__ = "graph_data"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    tags_content = Column(String, index=True, nullable=False)
-    articles = relationship(
-        "GeneratedArticles", secondary=article_tags, back_populates="tags"
+    #does_graph_exist = Column(Boolean, index=True, nullable=False)
+    graph_type = Column(String, index=True, nullable=False)
+    graph_labels = Column(JSON, nullable=False)
+    graph_values = Column(JSON, nullable=False)
+
+    article = relationship(
+        "GeneratedArticles",
+        back_populates="graph_data",
+        uselist=False
     )
+
 
 
 """class Images(Base):

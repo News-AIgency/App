ed784a2478382b0c0ca45cdc7755abcce4abf59e
JakubGasparin <55839847+JakubGasparin@users.noreply.github.com>
2025-04-19 19:15:39 +0200

    Update database.py
    database.py change

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,10 +1,21 @@
+import os
+
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-URL_DATABASE = "postgresql+asyncpg://[user]:[password]@localhost/[database name]"
+user = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
+database = os.environ.get("POSTGRES_DB")
+#print(user)
+#print(password)p
+#print(database)
+
+#URL_DATABASE = f"postgresql+asyncpg://{user}:{password}@localhost:15432/{database}" #the actual database
+URL_DATABASE = "postgresql+asyncpg://postgres:kubko123@localhost/new_db" # local testing database
 engine = create_async_engine(URL_DATABASE)
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
+

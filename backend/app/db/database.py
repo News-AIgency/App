0e9b757727d2b4c0478e3cad7bc1b19f003f0b97
TomasTomcany <61255352+Smitomic@users.noreply.github.com>
2025-04-20 15:06:05 +0200

    Fix localhost to env variables

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = "postgresql+asyncpg://[user]:[password]@localhost/[database name]"
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+URL_DATABASE = f"postgresql+asyncpg://[user]:[password]@{POSTGRES_HOST}/[database name]"
 engine = create_async_engine(URL_DATABASE)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession

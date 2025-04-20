0132cd3e5912a6c9b3cc7bab68a1c9128c27f000
LuborKoka <56118747+LuborKoka@users.noreply.github.com>
2025-04-20 16:07:12 +0200

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -10,12 +10,13 @@ user = os.environ.get("POSTGRES_USER")
 password = os.environ.get("POSTGRES_PASSWORD")
 database = os.environ.get("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "15432")
 
 #print(user)
 #print(password)p
 #print(database)
 
-URL_DATABASE = f"postgresql+asyncpg://{user}:{password}@{POSTGRES_HOST}:15432/{database}" #the actual database
+URL_DATABASE = f"postgresql+asyncpg://{user}:{password}@{POSTGRES_HOST}:{POSTGRES_PORT}/{database}" #the actual database
 #URL_DATABASE = "postgresql+asyncpg://postgres:kubko123@localhost/new_db" # local testing database
 engine = create_async_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
